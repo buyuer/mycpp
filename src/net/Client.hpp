0be2923e48ba99266784e5991e656d009b5ee774
buyuer <dingddding@163.com>
2020-04-10 17:20:18 +0800

    添加了Client.hpp文件

---
+++ src/net/Client.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace my{
+
+    class Client{
+
+    };
+
+}
--- src/net/net.hpp
+++ src/net/net.hpp
@@ -3,5 +3,6 @@
 #include "net/Socket.hpp"
 #include "net/Http.hpp"
 #include "net/Server.hpp"
+#include "net/Client.hpp"
 #include "net/HttpServer.hpp"
 #include "net/HttpClient.hpp"
