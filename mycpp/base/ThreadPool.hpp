c50439ab46d14bbab14ea9896634afced739bdd1
buyuer <dingddding@163.com>
2022-09-02 19:10:09 +0800

    添加线程池测试
    Signed-off-by: buyuer <dingddding@163.com>

--- mycpp/base/ThreadPool.hpp
+++ mycpp/base/ThreadPool.hpp
@@ -2,66 +2,88 @@
 
 #include "base/interface.hpp"
 
-namespace my{
+namespace my {
 
-    class ThreadPool{
+    class ThreadPool {
 
     private:
 
-        using Worker = void (*)(void);
+        using ThreadID = std::thread::id;
+        using Worker = void (*)(void *);
 
-        struct element{
+        struct element {
             Worker f;
+            void *arg;
+        };
+
+        enum Flag : uint32_t {
+            FLAG_NONE = 0,
+            FLAG_EXIT = 0x01,
         };
 
         std::mutex m_tasks;
         std::mutex m_cv;
         std::condition_variable cv;
         std::queue<element> tasks;
+        std::vector<std::jthread> pool;
+        uint32_t flags{};
 
-        static void threadf(ThreadPool* own){
 
-            while (true){
+        static void thread_entry(ThreadPool *own) {
+
+            while (true) {
+                if (own->flags bitand FLAG_EXIT) {
+                    return;
+                }
 
                 std::unique_lock<std::mutex> ul(own->m_tasks);
-
-                if(own->tasks.empty()){
+                if (own->tasks.empty()) {
                     std::unique_lock<std::mutex> ul_cv(own->m_cv);
                     ul.unlock();
                     own->cv.wait(ul_cv);
-                }
-                else{
+                } else {
                     auto temp = own->tasks.back();
                     Worker now = temp.f;
                     own->tasks.pop();
                     ul.unlock();
-                    now();
+                    now(temp.arg);
                 }
-
             }
 
         }
 
+        void destroy() {
+            for (auto &t: pool) {
+                auto stop = t.get_stop_source();
+                auto token = t.get_stop_token();
+
+                std::cout << t.request_stop() << std::endl;
+
+                flags |= FLAG_EXIT;
+
+                cv.notify_all();
+            }
+        }
+
     public:
-        ThreadPool(s32 count){
-            for(auto i = 0; i < count; i++){
-                std::thread t(my::ThreadPool::threadf, this);
+        explicit ThreadPool(uint32_t count = std::thread::hardware_concurrency()) {
+            pool.reserve(count);
+
+            for (uint32_t i = 0; i < count; i++) {
+                auto &t = pool.emplace_back(std::jthread(thread_entry, this));
                 t.detach();
             }
         }
 
-        template<typename Runable, typename ...Args>
-        void run(Runable runable, Args ...args){
-            /*my::ThreadPool::Worker f_ = [=](void)->void{
-                runable(args...);
-            };*/
-            /*element e{f_};
+        ~ThreadPool() {
+            destroy();
+        };
+
+        void run(Worker worker, void *arg) {
             std::unique_lock<std::mutex> ul(this->m_tasks);
-            this->tasks.push(e);
+            tasks.push({.f = worker, .arg = arg});
             ul.unlock();
-            this->cv.notify_one();*/
+            cv.notify_one();
         }
-
     };
-
 }
